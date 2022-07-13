d567062cd13c9cbe70b9fe75fde18ff587929325
mikevive <mikevive327@gmail.com>
2022-07-12 21:17:47 -0500

    Initial commit

---
+++ src/__main__.py
@@ -0,0 +1,15 @@
+"""Phone registers.
+
+Study of data structures and good programming practices conducted over a mock
+collection of phone records.
+"""
+
+
+def create_app():
+    """Create the app."""
+    print("App created! 🔥")
+
+
+if __name__ == "__main__":
+    """Execute only if run as the entry point into the program."""
+    create_app()
